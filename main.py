85cff83a2c9b4468db59e131bd6a1b026f55e0f2
Chris Melvin <lonestarr.xyz@gmail.com>
2024-01-23 16:01:10 -0500

    switched to height for x-axis and filtering off everything without hashrate

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ MONGO_URI = os.environ.get('MONGO_URI')
 
 st.title("Bitcoin Mempool Dashboard")
 
-st.write("Mempool size and hash rate over time")
+st.write("Mempool transaction count and hash rate over time")
 
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client['mempool']
 collection = db['blockheight']
-fields_to_include = {"_id": 0, "count": 1, "vsize": 1, "ts": 1}
-# result = collection.find({"height": {"$gt": 827023}}, fields_to_include)
-result = collection.find('', fields_to_include)
-st.line_chart(result, x="ts", y=["count", "vsize"])
+fields_to_include = {"_id": 0, "count": 1, "height": 1, "hashrate": 1}
+result = collection.find({"height": {"$gt": 827042}}, fields_to_include)
+# result = collection.find('', fields_to_include)
+st.line_chart(result, x="height", y=["count", "hashrate"])
+
 
 mongo_client.close()
