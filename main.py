79380e71da358e65a324ce339f4652cecdb6456f
Chris Melvin <lonestarr.xyz@gmail.com>
2024-01-23 14:48:24 -0500

    removed filter to show all the data I have

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ mongo_client = MongoClient(MONGO_URI)
 db = mongo_client['mempool']
 collection = db['blockheight']
 fields_to_include = {"_id": 0, "count": 1, "vsize": 1, "ts": 1}
-result = collection.find({"height": {"$gt": 827023}}, fields_to_include)
+# result = collection.find({"height": {"$gt": 827023}}, fields_to_include)
+result = collection.find('', fields_to_include)
 st.line_chart(result, x="ts", y=["count", "vsize"])
 
 mongo_client.close()
