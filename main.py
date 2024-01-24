11af4298851dad4598003a1c6ced209c4e81957f
Chris Melvin <lonestarr.xyz@gmail.com>
2024-01-23 22:45:21 -0500

    added minimum fee chart

--- main.py
+++ main.py
@@ -15,10 +15,15 @@ st.write("Mempool transaction count and hash rate over time")
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client['mempool']
 collection = db['blockheight']
-fields_to_include = {"_id": 0, "count": 1, "height": 1, "hashrate": 1}
+fields_to_include = {"_id": 0, "count": 1, "ts": 1, "hashrate": 1}
 result = collection.find({"height": {"$gt": 827042}}, fields_to_include)
 # result = collection.find('', fields_to_include)
-st.line_chart(result, x="height", y=["count", "hashrate"])
+st.line_chart(result, x="ts", y=["count", "hashrate"])
 
+st.write("Minimum fees over time")
+
+fields_to_include = {"_id": 0, "ts": 1, "minimumFee": 1}
+result = collection.find({"height": {"$gt": 827042}}, fields_to_include)
+st.line_chart(result, x="ts", y="minimumFee")
 
 mongo_client.close()
