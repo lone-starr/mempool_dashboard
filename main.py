d46e1edf882fec64d5df2168ef4ec72342e04324
Chris Melvin <lonestarr.xyz@gmail.com>
2024-01-24 13:47:31 -0500

    Added fastest fee and filtering from when I added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import json
+from datetime import datetime
 
 load_dotenv(".env")
 
@@ -10,7 +11,7 @@ MONGO_URI = os.environ.get('MONGO_URI')
 
 st.title("Bitcoin Mempool Dashboard")
 
-st.write("Mempool transaction count and hash rate over time")
+st.write("Mempool Transaction Count and Hash Rate over time")
 
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client['mempool']
@@ -20,10 +21,11 @@ result = collection.find({"height": {"$gt": 827042}}, fields_to_include)
 # result = collection.find('', fields_to_include)
 st.line_chart(result, x="ts", y=["count", "hashrate"])
 
-st.write("Minimum fees over time")
+st.write("Fastest and Minimum fees over time")
 
-fields_to_include = {"_id": 0, "ts": 1, "minimumFee": 1}
-result = collection.find({"height": {"$gt": 827042}}, fields_to_include)
-st.line_chart(result, x="ts", y="minimumFee")
+fields_to_include = {"_id": 0, "ts": 1, "minimumFee": 1, "fastestFee": 1}
+result = collection.find({"ts": {"$gt": datetime.fromisoformat(
+    '2024-01-24T18:20:21.764+00:00')}}, fields_to_include)
+st.line_chart(result, x="ts", y=["minimumFee", "fastestFee"])
 
 mongo_client.close()
