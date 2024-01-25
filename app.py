f740e3afb3e3ba9942edb932108ca15b947e0292
Chris Melvin <lonestarr.xyz@gmail.com>
2024-01-25 13:38:00 -0500

    changed queries to make names cleaner

--- app.py
+++ app.py
@@ -16,17 +16,35 @@ st.write("Mempool Transaction Count and Hash Rate over time")
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client['mempool']
 collection = db['blockheight']
-fields_to_include = {"_id": 0, "count": 1, "ts": 1, "hashrate": 1}
+fields_to_include = {"_id": 0, "count": 1, "time": "$ts", "hashrate": 1}
 result = collection.find({"height": {"$gt": 827042}}, fields_to_include)
-# result = collection.find('', fields_to_include)
-st.line_chart(result, x="ts", y=["count", "hashrate"])
+result = collection.find({"ts": {"$gt": datetime.fromisoformat(
+    '2024-01-24T18:20:21.764+00:00')}},   {
+    "_id": 0,
+    "time": {
+        "$dateAdd": {
+            "startDate": "$ts",
+            "unit": "hour",
+            "amount": -5
+        }
+    }, "tx count": "$count", "hashrate": 1
+})
+st.line_chart(result, x="time", y=["tx count", "hashrate"])
 
 st.write("Fastest and Minimum fees over time")
 
-fields_to_include = {"_id": 0, "ts": 1,
-                     "minimumFee": 1, "fastestFee": 1, "hourFee": 1}
 result = collection.find({"ts": {"$gt": datetime.fromisoformat(
-    '2024-01-24T18:20:21.764+00:00')}}, fields_to_include)
-st.line_chart(result, x="ts", y=["minimumFee", "fastestFee", "hourFee"])
+    '2024-01-24T18:20:21.764+00:00')}},   {
+    "_id": 0,
+    "time": {
+        "$dateAdd": {
+            "startDate": "$ts",
+            "unit": "hour",
+            "amount": -5
+        }
+    }, "min fee": "$minimumFee", "fast fee": "$fastestFee", "hour fee": "$hourFee"
+})
+
+st.line_chart(result, x="time", y=["min fee", "fast fee", "hour fee"])
 
 mongo_client.close()
