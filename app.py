4f54c50e9be4250b2acf1471e23dc299513f87fc
Chris Melvin <lonestarr.xyz@gmail.com>
2024-01-29 16:09:37 -0500

    Added mempool size and cleaned up some unused code

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ st.write("Mempool Transaction Count and Hash Rate over time")
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client['mempool']
 collection = db['blockheight']
-fields_to_include = {"_id": 0, "count": 1, "time": "$ts", "hashrate": 1}
-result = collection.find({"height": {"$gt": 827042}}, fields_to_include)
 result = collection.find({"ts": {"$gt": datetime.fromisoformat(
     '2024-01-24T18:20:21.764+00:00')}},   {
     "_id": 0,
@@ -26,10 +24,10 @@ result = collection.find({"ts": {"$gt": datetime.fromisoformat(
             "startDate": "$ts",
             "unit": "hour",
             "amount": -5
-        }
-    }, "tx count": "$count", "hashrate": 1
+        }},
+    "mempool size": {"$divide": ["$vsize", 1000]}, "tx count": "$count", "vsize": 1, "hashrate": 1
 })
-st.line_chart(result, x="time", y=["tx count", "hashrate"])
+st.line_chart(result, x="time", y=["tx count", "mempool size", "hashrate"])
 
 st.write("Fastest and Minimum fees over time")
 
