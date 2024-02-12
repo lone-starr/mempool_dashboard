33039bb5c683893c2e92599a3608e91f0cc81027
Chris Melvin <lonestarr.xyz@gmail.com>
2024-02-11 23:23:33 -0500

    Organized and added comments

--- app.py
+++ app.py
@@ -11,11 +11,12 @@ MONGO_URI = os.environ.get('MONGO_URI')
 
 st.title("Bitcoin Mempool Dashboard")
 
-st.write("Mempool Transaction Count and Hash Rate over time")
-
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client['mempool']
 collection = db['blockheight']
+
+# Mempool / Hashrate chart
+st.write("Mempool Transaction Count and Hash Rate over time")
 result = collection.find({"ts": {"$gt": datetime.fromisoformat(
     '2024-01-24T18:20:21.764+00:00')}},   {
     "_id": 0,
@@ -29,8 +30,8 @@ result = collection.find({"ts": {"$gt": datetime.fromisoformat(
 })
 st.line_chart(result, x="time", y=["tx count", "mempool size", "hashrate"])
 
+# Fee chart
 st.write("Fastest and Minimum fees over time")
-
 result = collection.find({"ts": {"$gt": datetime.fromisoformat(
     '2024-01-24T18:20:21.764+00:00')}},   {
     "_id": 0,
@@ -42,11 +43,10 @@ result = collection.find({"ts": {"$gt": datetime.fromisoformat(
         }
     }, "min fee": "$minimumFee", "fast fee": "$fastestFee", "hour fee": "$hourFee"
 })
-
 st.line_chart(result, x="time", y=["min fee", "fast fee", "hour fee"])
 
+# Price chart
 st.write("Bitcoin conversion rate in major currencies")
-
 result = collection.find({"ts": {"$gte": datetime.fromisoformat(
     '2024-02-05T22:29:27.182+00:00')}},   {
     "_id": 0,
@@ -58,7 +58,6 @@ result = collection.find({"ts": {"$gte": datetime.fromisoformat(
         }
     }, "USD": "$priceUSD", "EUR": "$priceEUR", "GBP": "$priceGBP", "CAD": "$priceCAD"
 })
-
 st.line_chart(result, x="time", y=["USD", "EUR", "GBP", "CAD"])
 
 mongo_client.close()
