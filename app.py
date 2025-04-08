ce0acbd03135f4ee785a020a077e93126d25eb76
lone-starr <lonestarr.xyz@gmail.com>
2025-04-08 16:58:42 -0400

    70-100

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ result = collection.find({"ts": {"$gte": datetime.now() - delta}},   {
 # st.line_chart(result, x="time", y=["USD"])
 resultDF = DataFrame(result)
 altair_chart = alt.Chart(resultDF).mark_line(
-    interpolate='step-after').encode(x=alt.X('time'), y=alt.Y('USD', scale=alt.Scale(domain=[50000, 100000])))
+    interpolate='step-after').encode(x=alt.X('time'), y=alt.Y('USD', scale=alt.Scale(domain=[70000, 100000])))
 
 st.altair_chart(altair_chart, use_container_width=True)
 # st.divider()
